769ff53cdbf898b8c0928c9ffee0fc63e1237ad5
dgvalerio <al.davigvalerio@gmail.com>
2023-01-27 00:20:32 -0300

    feat(project): add prisma

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from '@/app.module';
+import { PrismaService } from '@/prisma.service';
 
 const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const prismaService = app.get(PrismaService);
+
+  await prismaService.enableShutdownHooks(app);
 
   await app.listen(3000);
 };
