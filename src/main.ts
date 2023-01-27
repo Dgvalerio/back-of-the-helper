dc9b90143ec8a0a9acf0e905fad892c75e128e83
dgvalerio <al.davigvalerio@gmail.com>
2023-01-26 22:09:14 -0300

    feat(project): add graphql and class validator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from '@/app.module';
 
 const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 };
