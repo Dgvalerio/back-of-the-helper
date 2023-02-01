ef4bbf80d52dcf26b25dd6a29daad66b67a73c2e
dgvalerio <al.davigvalerio@gmail.com>
2023-01-31 21:35:04 -0300

    build(railway): update main to use port from env

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { PrismaService } from '@/prisma.service';
 
+import * as process from 'process';
+
 const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
 
@@ -13,7 +15,7 @@ const bootstrap = async (): Promise<void> => {
 
   await prismaService.enableShutdownHooks(app);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 };
 
 void bootstrap();
